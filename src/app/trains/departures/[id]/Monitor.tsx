465842e9a099c24620149c427c2005114cfeec4f
gab <mail@g3b.dev>
2024-11-10 10:28:50 +0100

    handle missing image

--- src/app/trains/departures/[id]/Monitor.tsx
+++ src/app/trains/departures/[id]/Monitor.tsx
@@ -5,10 +5,15 @@ import Image from "next/image";
 export function Monitor({monitor}: { monitor: any }) {
     const rows = monitor.trains.map((train: any, index: number) => (<Table.Tr key={index}>
         <Table.Td tt="capitalize">{train.company.toLowerCase()}</Table.Td>
-        <Table.Td><Image src={train.category.trim()} width={60} height={40} style={{
-            filter: "grayscale(100%)",
-
-        }} alt="Treno" /></Table.Td>
+        <Table.Td w={24}>
+            {train.category && (
+                <Image src={train.category.trim()} width={50} height={50} style={{
+                    filter: "grayscale(100%)",
+                    width: "100%",
+                    height: "100%",
+                }} alt="Treno" />
+            )}
+        </Table.Td>
         <Table.Td>{train.trainNumber}</Table.Td>
         <Table.Td>{train.destination}</Table.Td>
         <Table.Td>{train.departureTime}</Table.Td>
@@ -17,7 +22,7 @@ export function Monitor({monitor}: { monitor: any }) {
         <Table.Td>{train.departing && "IN PARTENZA"}</Table.Td>
     </Table.Tr>));
 
-    return (<Table striped highlightOnHover stickyHeader verticalSpacing="sm" captionSide="bottom" ta="left">
+    return (<Table striped highlightOnHover stickyHeader captionSide="bottom" ta="left">
         <Table.Caption fz="md">{monitor.alerts.trim()}</Table.Caption>
         {monitor.trains.length > 0 ? (
             <Table.Thead>
