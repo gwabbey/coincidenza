f90aa71b6269bfdcd61cad0d917f1e345e875e2c
gab <mail@g3b.dev>
2024-11-10 12:47:25 +0100

    add title in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 'use client';
-import {ActionIcon, Flex, useMantineColorScheme} from "@mantine/core";
+import {ActionIcon, Flex, Text, useMantineColorScheme} from "@mantine/core";
 import {IconMoon, IconSun} from "@tabler/icons-react";
+import Link from "next/link";
 
 export function Header() {
     const {colorScheme, toggleColorScheme} = useMantineColorScheme();
 
     return (
-        <Flex align="center" justify="end" pt="md" pr="md">
+        <Flex align="center" justify="space-between" pt="md" px="md">
+            <Text component={Link} href="https://trasporti.g3b.dev" size="lg" fw={200}
+                  fs="italic">trasporti.g3b.dev</Text>
             <ActionIcon
                 onClick={toggleColorScheme}
                 variant="default"
