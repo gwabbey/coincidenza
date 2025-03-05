a988b1db14a2af7edd949fe53b7d277611a4a8da
gab <mail@g3b.dev>
2025-03-06 00:07:07 +0100

    ooooh

--- src/app/directions/layout.tsx
+++ src/app/directions/layout.tsx
@@ -4,18 +4,18 @@ export default async function Layout({
     children: React.ReactNode;
 }) {
     const OTP_SERVER_IP = process.env.OTP_SERVER_IP || "localhost:8080";
-    try {
-        await fetch(`http://${OTP_SERVER_IP}/otp`);
 
-        return children;
-    } catch (e) {
-        console.log(e)
-        return (
-            <div className="flex flex-col gap-4 text-center">
-                <h1 className="text-2xl font-bold">oh no</h1>
-                <div className="text-2xl">il servizio non funziona al momento... 💔</div>
-                <div>riprova tra qualche minuto</div>
-            </div>
-        )
-    }
+    let isServerAvailable = false;
+    try {
+        const res = await fetch(`http://${OTP_SERVER_IP}/otp`, { cache: "no-store" });
+        if (res.ok) isServerAvailable = true;
+    } catch (e) { }
+
+    return isServerAvailable ? children : (
+        <div className="flex flex-col gap-4 text-center">
+            <h1 className="text-2xl font-bold">oh no</h1>
+            <div className="text-2xl">il servizio non funziona al momento... 💔</div>
+            <div>riprova tra qualche minuto</div>
+        </div>
+    );
 }
