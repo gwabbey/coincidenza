be8697e76f679772e651952e8f68eaf3e69bc1bd
gab <mail@g3b.dev>
2025-12-17 18:28:26 +0100

    fix geo

--- src/components/geolocation.tsx
+++ src/components/geolocation.tsx
@@ -2,13 +2,14 @@
 
 export function getUserLocation(): Promise<{ lat: number; lon: number }> {
     return new Promise((resolve, reject) => {
-        if (typeof window !== 'undefined' && 'geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    resolve({lat: position.coords.latitude, lon: position.coords.longitude});
-                },
-                {enableHighAccuracy: true, maximumAge: 0}
-            );
-        } else reject(new Error('Geolocation not supported'));
+        if (!('geolocation' in navigator)) {
+            reject(new Error('Geolocation not supported'));
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(pos => resolve({
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
+        }), err => reject(err), {enableHighAccuracy: true, maximumAge: 0});
     });
 }
